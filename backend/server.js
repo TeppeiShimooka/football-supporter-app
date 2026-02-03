9e230fb757975bb33323903533798b1485fce340
TeppeiShimooka <st18football@outlook.jp>
2026-02-03 16:52:03 +0900

    create:バックエンドのサーバーを立ち上げて、APIを接続

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,24 @@ const API_KEY = process.env.FOOTBALL_API_KEY;
 
 const app = express();
 app.use(cors());
+
+app.get('/api/matches' , async (req , res)=>{
+    try{
+        const response = await axios.get('https://api.football-data.org/v4/competitions/PL/matches' , {
+            headers:{'X-Auth-Token':API_KEY},
+            params:{
+            //     status:'SCHEDULED' ,
+            //     // matchday:23
+            }
+        });
+        res.json(response.data);
+    }catch(error){
+        console.log("http error");
+    }
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
--- frontend/script.js
+++ frontend/script.js
@@ -6,5 +6,22 @@ const sliderEvent = menuBtn.addEventListener('click' , ()=>{
     slider.classList.toggle("hidden");
 });
 
+const API_URL = 'http://localhost:3000/api/matches';
+
+const fetchMatches = async ()=>{
+    try{
+        const response = await fetch(API_URL);
+        if(!response.ok) throw new Error('Network response was not ok');
+
+        const data = response.json();
+        console.log('取得したデータ:' , data);
+
+    }catch (error){
+        console.log('データの取得に失敗した:' , error)
+    }
+}
+
+fetchMatches();
+
 
 
